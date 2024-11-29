a4c626865b37b0539b4f5fecfd7e1acd28f60876
Vadim-IT <khc.vadim.it@mail.ru>
2024-11-29 15:32:04 +0300

    The task is exploding cubes
    The solution to the problem of Exploding cubes

---
+++ Assets/03_Explosions_of_cubes/Scripts/ChooseObject.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ChooseObject : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Ray _ray;
+        
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject objectHit = FindObject();
+
+            if (objectHit.GetComponent<Divider>())
+            {
+                objectHit.GetComponent<Divider>().UseActions();
+            }
+        }
+    }
+
+    private GameObject FindObject()
+    {
+        GameObject objectHit = null;
+
+        _ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(_ray.origin, _ray.direction);
+        if (Physics.Raycast(_ray, out hit, Mathf.Infinity))
+        {
+            objectHit = hit.transform.gameObject;
+        }
+
+        return objectHit;
+    }
+}
---
+++ Assets/03_Explosions_of_cubes/Scripts/Divider.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Divider : MonoBehaviour
+{
+    private float radius = 40f;
+    private float force = 1000f;
+
+    public float ChanceSeparation = 100;
+
+    public void UseActions()
+    {
+        Destroy(gameObject);
+        Separation();
+    }
+
+    private void Separation()
+    {
+        int minValue = 0;
+        int maxValue = 101;
+        int value = Random.Range(minValue, maxValue);
+
+        if (value < ChanceSeparation)
+            CreateCubes();
+    }
+
+    private void CreateCubes()
+    {
+        int minValue = 2;
+        int maxValue = 7;
+        int value = Random.Range(minValue, maxValue);
+        Material material = gameObject.GetComponent<Renderer>().material;
+        List<Collider> explodeColliders = new List<Collider>();
+
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x / 2f, gameObject.transform.localScale.y / 2f, gameObject.transform.localScale.z / 2f);
+        gameObject.GetComponent<Divider>().ChanceSeparation /= 2;
+
+        for (int i = 0; i < value; i++)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+            explodeColliders.Add(Instantiate(gameObject).GetComponent<Collider>());
+        }
+
+        Explode(explodeColliders);
+    }
+
+    private void Explode(List<Collider> explodeColliders)
+    {
+        for (int i = 0; i < explodeColliders.Count; i++)
+        {
+            Rigidbody rigidbody = explodeColliders[i].attachedRigidbody;
+            if (rigidbody)
+            {
+                rigidbody.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+    }
+}
