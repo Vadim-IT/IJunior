6d1e4c165c6ea33b46f9ca22d30c55e541574c4b
Vadim-IT <khc.vadim.it@mail.ru>
2024-11-30 11:08:54 +0300

    Cube Explosions - Bug fixes
    Cube Explosions - Bug fixes

--- Assets/03_Explosions_of_cubes/Scripts/Divider.cs
+++ Assets/03_Explosions_of_cubes/Scripts/Divider.cs
@@ -29,15 +29,16 @@ public class Divider : MonoBehaviour
         int minValue = 2;
         int maxValue = 7;
         int value = Random.Range(minValue, maxValue);
-        Material material = gameObject.GetComponent<Renderer>().material;
-        List<Collider> explodeColliders = new List<Collider>();
+        List<Collider> explodeColliders = new List<Collider>();        
 
         gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x / 2f, gameObject.transform.localScale.y / 2f, gameObject.transform.localScale.z / 2f);
         gameObject.GetComponent<Divider>().ChanceSeparation /= 2;
 
         for (int i = 0; i < value; i++)
         {
-            gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+            Material material = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+            material.color = Random.ColorHSV();
+            gameObject.GetComponent<Renderer>().material = material;
             explodeColliders.Add(Instantiate(gameObject).GetComponent<Collider>());
         }
 
